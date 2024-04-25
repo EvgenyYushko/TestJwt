1df0af5841ab77a9d7a4caf0088ea0a0926e0483
evgeny <jeka-krut@mail.ru>
2024-04-25 21:26:03 +0300

    +add unit test

---
+++ Tests/JwtAuthentication.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace JwtAuthentication.Test
+{
+	[TestFixture]
+	public class FormHelperTests
+	{
+		[Test]
+		public void GetFormTextCaption_GenerateFormTextCaption_TextCaption()
+		{
+			// Arrange
+			var headerText = "Test";
+			var bodyText = "Test2";
+			var expected = $"{headerText} {bodyText}";
+
+			// Act
+			var actual = true;
+
+			// Assert
+			Assert.That("a" == "a");
+		}
+	}
+}
+
